3a20232ff4e3550f73aa5f61fc3b8ba509811874
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-05-18 17:40:16 +0300

    Create Sort and Star.py

---
+++ Sort and Star.py
@@ -0,0 +1,8 @@
+# def two_sort(arr):
+#     arr.sort()
+#     first_word = arr[0]
+#     result = '***'.join(first_word)
+#     return result
+        
+
+# print(two_sort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
