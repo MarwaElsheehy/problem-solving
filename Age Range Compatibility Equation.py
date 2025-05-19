4360799d62327b67759950cbf3c7cabe270be086
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-05-19 20:38:02 +0300

    Create Age Range Compatibility Equation.py

---
+++ Age Range Compatibility Equation.py
@@ -0,0 +1,12 @@
+# import math
+# def dating_range(age):
+    
+#     if age > 14:
+#         min_num = math.floor((age / 2) + 7)
+#         max_num = math.floor(2 * (age - 7))
+#     else:
+#         min_num = math.floor(age - 0.10 * age)
+#         max_num = math.floor(age + 0.10 * age)
+
+#     return f"{min_num} - {max_num}"
+# print(dating_range(12))
