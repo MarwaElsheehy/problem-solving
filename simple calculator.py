f7d6ac6e49301db18716fd866307bbd5142c6457
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-05-19 20:52:03 +0300

    Create simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,13 @@
+def calculator(x, y, op):
+    if op == "+":
+        return x + y
+    elif op == "-":
+        return x - y
+    elif op == "*":
+        return x * y
+    elif op == "/":
+        return x / y
+    else:
+        return "unknown value"
+    
+print(calculator(6, 2, '+'))
