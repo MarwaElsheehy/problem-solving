e1c95b36686f927193aec2ea8a775572d65b872f
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-05-19 20:45:34 +0300

    Create Removing Elements.py

---
+++ Removing Elements.py
@@ -0,0 +1,9 @@
+# def remove_every_other(my_list):
+#   res = []
+  
+#   for index, value in enumerate(my_list):
+#     if index % 2 == 0:
+#       res.append(value)
+#   return res
+
+# print(remove_every_other(["Keep", "Remove", "Keep", "Remove", "Keep"]))
