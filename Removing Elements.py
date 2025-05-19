79731eff13773da1e0052f13f5d56f9ce121fdeb
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-05-19 20:46:21 +0300

    Update Removing Elements.py

--- Removing Elements.py
+++ Removing Elements.py
@@ -1,9 +1,9 @@
-# def remove_every_other(my_list):
-#   res = []
+def remove_every_other(my_list):
+  res = []
   
-#   for index, value in enumerate(my_list):
-#     if index % 2 == 0:
-#       res.append(value)
-#   return res
+  for index, value in enumerate(my_list):
+    if index % 2 == 0:
+      res.append(value)
+  return res
 
-# print(remove_every_other(["Keep", "Remove", "Keep", "Remove", "Keep"]))
+print(remove_every_other(["Keep", "Remove", "Keep", "Remove", "Keep"]))
