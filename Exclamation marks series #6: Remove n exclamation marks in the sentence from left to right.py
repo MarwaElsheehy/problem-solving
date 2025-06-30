df733b88fdf2cee991d942e3e04aff0670ae0306
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-06-30 22:10:13 +0300

    Update Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right.py

--- Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right.py
+++ Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right.py
@@ -1,4 +1,15 @@
 def remove(s, n):
+    result = []
+    for char in s:
+        if char == '!' and n > 0:
+            n -= 1
+        else:
+            result.append(char)
+    return ''.join(result)
+
+print(remove("!!!Hi !!hi!!! !hi", 5))
+
+def remove(s, n):
     return s.replace('!', '', n)
 
 print(remove("!!!Hi !!hi!!! !hi", 5))
