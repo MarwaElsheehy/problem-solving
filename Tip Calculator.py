744dbeb9b89f31a912ee4f48cfb24efc98a5e6b9
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-06-30 23:13:22 +0300

    Create Tip Calculator.py

---
+++ Tip Calculator.py
@@ -0,0 +1,15 @@
+def calculate_tip(amount, rating):
+    tips = {
+        'terrible': 0,
+        'poor' : 0.05,
+        'good' : 0.1,
+        'great' : 0.15,
+        'excellent' : 0.2
+    }
+    if rating.lower() in tips:
+        return ceil(amount * tips[rating.lower()])
+    else:
+        return 'Rating not recognised'
+    
+
+print(calculate_tip(30, "poor"))
