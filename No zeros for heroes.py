baff6ff46a062ee732bae597459b0b8c30507f0d
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-06-30 23:09:05 +0300

    Create No zeros for heroes.py

---
+++ No zeros for heroes.py
@@ -0,0 +1,10 @@
+def no_boring_zeros(n):
+
+  if n == 0:
+    return 0
+  else:
+    return int(str(n).rstrip('0'))
+
+print(no_boring_zeros(960000))
+print(no_boring_zeros(1050))
+print(no_boring_zeros(0))
