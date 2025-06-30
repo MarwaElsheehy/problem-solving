b059ea2d522d98dca7a4afc905a2441ce60c2896
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-06-30 22:45:25 +0300

    Update Generate range of integers.py

--- Generate range of integers.py
+++ Generate range of integers.py
@@ -1,4 +1,16 @@
 def generate_range(start, stop, step):
+    result = []
+    while start <= stop:
+        result.append(start)
+        start += step
+    return result
+
+print(generate_range(1, 10, 1))
+print(generate_range(-10, 1, 1))
+print(generate_range(1, 15, 20))
+
+
+def generate_range(start, stop, step):
     
     return [i for i in range(start, stop + 1, step)]
 
