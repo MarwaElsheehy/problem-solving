164dd476a0431ad51fbc5cc9980d0d6f6058b9b7
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-06-30 22:58:40 +0300

    Create Holiday VI - Shark Pontoon.py

---
+++ Holiday VI - Shark Pontoon.py
@@ -0,0 +1,8 @@
+def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
+  if dolphin:
+    shark_speed /= 2
+  if shark_distance / shark_speed > pontoon_distance / you_speed:
+    return 'Alive!'
+  return 'Shark Bait!'
+
+print(shark(12, 50, 4, 8, True))
