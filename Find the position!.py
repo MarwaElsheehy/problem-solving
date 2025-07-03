617fd6be7d7d2f222597e88bea83ecdfcbf9f23e
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-07-03 23:17:35 +0300

    Update Find the position!.py

--- Find the position!.py
+++ Find the position!.py
@@ -1,6 +1,7 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 def position(letter):
-    return alphabet.index(letter) + 1
+    return alphabet.index(letter.lower()) + 1
 
 print(position("a"))
 print(position("z"))
+print(position("B"))
