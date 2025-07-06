bfb94b1f718753df9cb47840e5e03e01ca48c45a
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2025-07-06 22:23:44 +0300

    Create Grasshopper - Check for factor.py

---
+++ Grasshopper - Check for factor.py
@@ -0,0 +1,8 @@
+def check_for_factor(base, factor):
+    if base % factor == 0:
+        return True
+    else:
+        return False
+
+print(check_for_factor(10, 2))
+print(check_for_factor(9, 2))
